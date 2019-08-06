f7d5e718c056e1a24dc8513923e404c802aa1b78
Thijs Janzen <thijsjanzen@gmail.com>
2019-08-06 11:17:39 +0200

    revert param_from_prior

--- R/param_from_prior.R
+++ R/param_from_prior.R
@@ -1,18 +1,15 @@
 #' draw parameter combinations from the prior
-#' @param model requested water model, default is to sample from 1:3
+#' @param empty_input empty vector, used to facilitate usage of apply
 #' @return vector with 7 entries: extinction, sympatric speciation at high water, sympatric speciation at low water, allopatric speciation, amount of perturbation, the chosen water model and the abc-weight
 #' @export
-param_from_prior <- function(model = NULL) {
+param_from_prior <- function(empty_input) {
   output <- rep(NA, 7)
   output[1] <- 10 ^ (-3 + 5 * stats::runif(1, 0, 1))  #extinction
   output[2] <- 10 ^ (-3 + 5 * stats::runif(1, 0, 1))  #symp spec high
   output[3] <- 10 ^ (-3 + 5 * stats::runif(1, 0, 1))  #symp spec low
   output[4] <- 10 ^ (-3 + 5 * stats::runif(1, 0, 1))  #allo spec
   output[5] <- 10 ^ (-3 + 3 * stats::runif(1, 0, 1))  #jiggle
-  output[6] <- model
-  if(is.null(output[6])) {
-    output[6] <- sample(1:3, 1)
-  }
+  output[6] <- sample(1:3, 1)
   output[7] <- 1
 
   return(output)
