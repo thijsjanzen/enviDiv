0137ed9068f351c050a8f9a5012759113768e280
Thijs Janzen <thijsjanzen@gmail.com>
2019-10-02 16:02:17 +0200

    update infer_params.R

--- R/infer_params.R
+++ R/infer_params.R
@@ -96,82 +96,72 @@ infer_params <- function(number_of_particles,
       if(fix_model != -1) candidate_particles[, 6] <- fix_model
 
       found_trees <- c()
-      if (num_cores == 1) {
-        found_trees <- apply(candidate_particles, 1, sim_envidiv_tree,
-                             crown_age, TRUE)
-      } else {
-
-        input <- lapply(1:nrow(candidate_particles), function(i) candidate_particles[i,])
-
-        found_trees <- future.apply::future_lapply(input,
-                                                   sim_envidiv_tree,
-                                                   crown_age, TRUE)
-      }
-
-      if (length(found_trees) > 0) {
-        stats <- list()
-        if(num_cores == 1) {
-          stats <- lapply(found_trees, calc_sum_stats, emp_tree)
-        } else {
-
-          stats <- future.apply::future_lapply(found_trees,
-                                               calc_sum_stats,
-                                               emp_tree)
-        }
-
-        stat_matrix <- matrix(unlist(stats, use.names = FALSE),
-                              ncol = 8,
-                              byrow = TRUE)
-
-        results <- cbind(candidate_particles, stat_matrix)
-
-        stat_matrix <- stat_matrix[!is.infinite(results[, 8]), ]
-        results <- results[!is.infinite(results[, 8]), ]
-
-        if(length(stat_matrix) > 0) {
-
-          local_fit <- apply(stat_matrix, 1, calc_fit, emp_stats)
-
-          results <- cbind(results, local_fit)
-
-          results <- results[local_fit < local_eps, ]
-          selected_fits <- local_fit[local_fit < local_eps]
-
-          next_par <- rbind(next_par, results)
-          remaining_particles <- number_of_particles - length(next_par[, 1])
-          accept_rate <- round(length(results[, 1]) / remaining_particles, 2)
-          if (!is.null(dim(results))) {
-            end_time <- Sys.time()
-            diff_time <- (end_time - start_time)[[1]]
 
 
-            cat(iter, "\t", remaining_particles, "\t",
-                length(results[, 1]), "\t",
-                accept_rate, "\t",
-                mean(selected_fits), "\t", mean(local_fit), "\t", diff_time, "\n")
-          }
+      input <- lapply(1:nrow(candidate_particles), function(i) candidate_particles[i,])
+
+      found_trees <- future.apply::future_lapply(input,
+                                                 sim_envidiv_tree,
+                                                 crown_age, TRUE)
+
+
+
+      stats <- future.apply::future_lapply(found_trees,
+                                           calc_sum_stats,
+                                           emp_tree)
+
+      stat_matrix <- matrix(unlist(stats, use.names = FALSE),
+                            ncol = 8,
+                            byrow = TRUE)
+
+      results <- cbind(candidate_particles, stat_matrix)
+
+      stat_matrix <- stat_matrix[!is.infinite(results[, 8]), ]
+      results <- results[!is.infinite(results[, 8]), ]
+
+      if(length(stat_matrix) > 0) {
+
+        local_fit <- apply(stat_matrix, 1, calc_fit, emp_stats)
+
+        results <- cbind(results, local_fit)
+
+        results <- results[local_fit < local_eps, ]
+        selected_fits <- local_fit[local_fit < local_eps]
+
+        next_par <- rbind(next_par, results)
+        remaining_particles <- number_of_particles - length(next_par[, 1])
+        accept_rate <- round(length(results[, 1]) / remaining_particles, 2)
+        if (!is.null(dim(results))) {
+          end_time <- Sys.time()
+          diff_time <- (end_time - start_time)[[1]]
+
+
+          cat(iter, "\t", remaining_particles, "\t",
+              length(results[, 1]), "\t",
+              accept_rate, "\t",
+              mean(selected_fits), "\t", mean(local_fit), "\t", diff_time, "\n")
         }
       }
     }
+  }
 
-    next_par <- next_par[1:number_of_particles, ]
-    # calculate weights
-    new_weights <- apply(next_par[, 1:6], 1, calculate_weight,
-                         previous_par[, 1:6], previous_par[, 7], sd_params)
-    new_weights <- new_weights / sum(new_weights)
-    next_par[, 7] <- new_weights
-    previous_par <- next_par
-    # write next par to file
-    colnames(next_par) <-
-      c("extinct", "sym_high", "sym_low", "allo", "jiggle", "model",
-        "weight",
-        "nltt", "gamma", "mbr", "num_lin",
-        "beta", "colless", "sackin", "ladder",
-        "fit")
-    next_par <- tibble::as_tibble(next_par)
-    if (write_to_file == TRUE) {
-      readr::write_tsv(next_par, path = paste0("iter_", iter, ".txt"))
-    }
+  next_par <- next_par[1:number_of_particles, ]
+  # calculate weights
+  new_weights <- apply(next_par[, 1:6], 1, calculate_weight,
+                       previous_par[, 1:6], previous_par[, 7], sd_params)
+  new_weights <- new_weights / sum(new_weights)
+  next_par[, 7] <- new_weights
+  previous_par <- next_par
+  # write next par to file
+  colnames(next_par) <-
+    c("extinct", "sym_high", "sym_low", "allo", "jiggle", "model",
+      "weight",
+      "nltt", "gamma", "mbr", "num_lin",
+      "beta", "colless", "sackin", "ladder",
+      "fit")
+  next_par <- tibble::as_tibble(next_par)
+  if (write_to_file == TRUE) {
+    readr::write_tsv(next_par, path = paste0("iter_", iter, ".txt"))
   }
   return(next_par)
 }
