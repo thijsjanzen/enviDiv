6993e8a75e053bd63f9b82b287888d0245a8382a
Thijs Janzen <thijsjanzen@gmail.com>
2019-11-01 15:10:59 +0100

    add read_reference_table

---
+++ R/read_reference_table.R
@@ -0,0 +1,30 @@
+#' collect reference table based on previously generated reference tables for each model
+#' @param path path of directory where each subdirectory [1, 2, 3] is located
+#' @return reference table
+#' @export
+read_reference_table <- function(path) {
+
+  reference_table <- c()
+  for(i in 1:3) {
+    file_name <- paste0("./", i, "/reference_table.txt")
+    vy <- readr::read_tsv(file_name, col_names = F)
+    colnames(vy)  <-
+      c("extinct", "sym_high", "sym_low", "allo", "jiggle", "model",
+        "weight",
+        "nltt", "gamma", "mbr", "num_lin",
+        "beta", "colless", "sackin", "ladder", "cherries", "ILnumber",
+        "pitchforks", "stairs",
+        "spectr_eigen", "spectr_asymmetry", "spectr_peakedness")
+
+    reference_table <- rbind(reference_table, vy)
+  }
+
+  focal_models <- as.factor(reference_table$model)
+
+  ref_table <- dplyr::select(reference_table, c("nltt", "gamma", "mbr", "num_lin",
+                                         "beta", "colless", "sackin", "ladder", "cherries", "ILnumber",
+                                         "pitchforks", "stairs",
+                                         "spectr_eigen", "spectr_asymmetry", "spectr_peakedness"))
+
+  return(ref_table)
+}
