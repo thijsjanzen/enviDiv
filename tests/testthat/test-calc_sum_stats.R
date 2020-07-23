0cce02259ebf08d2a51d1e577a026949c25cb974
Thijs Janzen <thijsjanzen@gmail.com>
2020-07-23 15:20:05 +0200

    Update test-calc_sum_stats.R

--- tests/testthat/test-calc_sum_stats.R
+++ tests/testthat/test-calc_sum_stats.R
@@ -2,20 +2,25 @@ context("calculate sum stats")
 
 test_that("usage", {
   params <- c(0, 0.5, 0, 0, 0, 1)
-  emp_tree <- sim_envidiv_tree(params, crown_age = 5, seed = 42)
+  emp_tree <- enviDiv::sim_envidiv_tree(params, crown_age = 5, seed = 42)
 
   while (is.null(emp_tree))
-    emp_tree <- sim_envidiv_tree(params, crown_age = 5)
+    emp_tree <- enviDiv::sim_envidiv_tree(params, crown_age = 5)
 
-  emp_tree2 <- sim_envidiv_tree(params, crown_age = 5, seed = 43)
+  emp_tree2 <- enviDiv::sim_envidiv_tree(params, crown_age = 5, seed = 43)
 
   while (is.null(emp_tree2))
-    emp_tree2 <- sim_envidiv_tree(params, crown_age = 5)
+    emp_tree2 <- enviDiv::sim_envidiv_tree(params, crown_age = 5)
 
-  v1 <- calc_sum_stats(emp_tree)
-  v2 <- calc_sum_stats(emp_tree2)
+  v1 <- enviDiv::calc_sum_stats(emp_tree)
+  cat(v1, "\n")
+  v2 <- enviDiv::calc_sum_stats(emp_tree2)
+  cat(v2, "\n")
 
   a <- sum(v1 != v2)
+  cat(a, "\n")
+  cat(beautier::get_crown_age(emp_tree), "\n")
+  cat(beautier::get_crown_age(emp_tree2), "\n")
   testthat::expect_true(a != 0)
   testthat::expect_equal(v1[[11]], 5)
   testthat::expect_equal(v2[[11]], 5)
